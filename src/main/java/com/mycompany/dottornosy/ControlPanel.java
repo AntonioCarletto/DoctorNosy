b7668c47688596dfd917ba177f6c358703cfe989
Antonio <antonio.carletto@gmail.com>
2017-10-13 19:37:10 +0200

    Progetto iniziale

---
+++ src/main/java/com/mycompany/dottornosy/ControlPanel.java
@@ -0,0 +1,13 @@
+package com.mycompany.dottornosy;
+
+/**
+ * Hello world!
+ *
+ */
+public class ControlPanel 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
